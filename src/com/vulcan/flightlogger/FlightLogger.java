f77e85bc2317a227e7d93f05d17767170509d3df
Michael Coyote <michaelco@vulcan.com>
2014-05-09 14:05:29 -0700

    main screen initial checkin

--- src/com/vulcan/flightlogger/FlightLogger.java
+++ src/com/vulcan/flightlogger/FlightLogger.java
@@ -4,6 +4,7 @@ import com.vulcan.flightlogger.altimeter.LaserAltimeterActivity;
 import com.vulcan.flightlogger.altimeter.SerialConsole;
 import com.vulcan.flightlogger.geo.GPSDebugActivity;
 import com.vulcan.flightlogger.geo.RouteListActivity;
+import com.vulcan.flightlogger.util.SquishyTextView;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -12,6 +13,10 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 
 public class FlightLogger extends Activity {
 
@@ -20,13 +25,86 @@ public class FlightLogger extends Activity {
 
 	private final String LOGGER_TAG = FlightLogger.class.getSimpleName();
 
+	void setupSquishyFontView(int groupID, int ideal, int min) {
+		SquishyTextView squishyTextView = (SquishyTextView) findViewById(groupID);
+		if (squishyTextView != null) {
+			squishyTextView.setIdealTextSizeDP(ideal);
+			squishyTextView.setMinimumTextSizeDP(min);
+		}
+	}
+
+	void setColorforViewWithID(int groupID, int colorID) {
+		View v = findViewById(groupID);
+		if (v != null)
+			v.setBackgroundColor(getResources().getColor(colorID));
+	}
+
+	void setBlackColorforViewWithID(int groupID) {
+		setColorforViewWithID(groupID, R.color.nav_background);
+	}
+
+	void setHeaderColorforViewWithID(int groupID) {
+		setColorforViewWithID(groupID, R.color.nav_header_bg);
+	}
+
+	void setFooterColorforViewWithID(int groupID) {
+		setColorforViewWithID(groupID, R.color.nav_footer_bg);
+	}
+
+	protected void setupColors() {
+		// whole screen
+		setBlackColorforViewWithID(R.id.main_layout);
+
+		// left & right block
+		setBlackColorforViewWithID(R.id.navscreenLeft);
+		setBlackColorforViewWithID(R.id.navscreenRight);
+
+		// altitude
+		setBlackColorforViewWithID(R.id.nav_altitude_group_wrapper);
+		setBlackColorforViewWithID(R.id.nav_altitude_group);
+		setBlackColorforViewWithID(R.id.nav_altitude_value);
+		setBlackColorforViewWithID(R.id.nav_altitude_righthalf);
+		setBlackColorforViewWithID(R.id.nav_altitude_label);
+		setBlackColorforViewWithID(R.id.nav_altitude_units);
+
+		// speed
+		setBlackColorforViewWithID(R.id.nav_speed_group_wrapper);
+		setBlackColorforViewWithID(R.id.nav_speed_group);
+		setBlackColorforViewWithID(R.id.nav_speed_value);
+		setBlackColorforViewWithID(R.id.nav_speed_righthalf);
+		setBlackColorforViewWithID(R.id.nav_speed_label);
+		setBlackColorforViewWithID(R.id.nav_speed_units);
+
+		// header
+		setHeaderColorforViewWithID(R.id.nav_header);
+		setHeaderColorforViewWithID(R.id.nav_header_left);
+		setHeaderColorforViewWithID(R.id.nav_header_right);
+		setHeaderColorforViewWithID(R.id.nav_header_settings_button);
+
+		// footer
+		setFooterColorforViewWithID(R.id.nav_footer);
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.main);
+		// TESTING Log.i("main", "onCreate!");
 
+		ViewGroup layout = (ViewGroup) findViewById(R.id.navscreenLeft);
+		TransectILSView tv = new TransectILSView(this);
+		LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT,
+				LayoutParams.MATCH_PARENT);
+		tv.setLayoutParams(lp);
+		layout.addView(tv);
+
+		setupSquishyFontView(R.id.nav_altitude_value, 190, 20);
+		setupSquishyFontView(R.id.nav_speed_value, 130, 20);
+
+		// TESTING flag - set to false for debugging layout
+		if (true)
+			setupColors();
 	}
 
 	/**
@@ -65,7 +143,7 @@ public class FlightLogger extends Activity {
 		}
 		return true;
 	}
-	
+
 	/**
 	 * Callbacks from activities that return results
 	 */
@@ -78,13 +156,12 @@ public class FlightLogger extends Activity {
 				String gpxName = data.getStringExtra("gpxfile");
 				Log.d(LOGGER_TAG, "GPX filename: " + gpxName);
 				Intent it = new Intent(this, RouteListActivity.class);
-	            it.putExtra("gpxfile", gpxName);
-	            startActivity(it);
+				it.putExtra("gpxfile", gpxName);
+				startActivity(it);
 			}
 		}
 	}
 
-	
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
@@ -93,6 +170,16 @@ public class FlightLogger extends Activity {
 	@Override
 	public void onResume() {
 		super.onResume();
+
+		// note: requires Android 4.4 / api level 16 & 19
+		View decorView = getWindow().getDecorView();
+		decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
 	}
 
 }
