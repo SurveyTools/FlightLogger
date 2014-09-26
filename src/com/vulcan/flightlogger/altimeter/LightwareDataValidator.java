b2ca45a7dce416b766863e06472bfe219dc4b4bc
Jay Lorenzo <cxplanet@hotmail.com>
2014-09-26 16:10:02 -0700

    Fix RNG issue

--- src/com/vulcan/flightlogger/altimeter/LightwareDataValidator.java
+++ src/com/vulcan/flightlogger/altimeter/LightwareDataValidator.java
@@ -1,14 +1,13 @@
 package com.vulcan.flightlogger.altimeter;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.util.Arrays;
 import android.util.Log;
 import com.vulcan.flightlogger.util.KMPMatch;
 
 public class LightwareDataValidator implements AltimeterValidator {
 	
-	final int dataSampleSize = 15;
+	final static int dataSampleBufferSize = 100;
+	final static int dataSampleSize = 10;
 	final byte[] terminatingPattern = { (byte)0x20, (byte)0x6d, (byte)0x0d, (byte)0x0a }; 
 
 	@Override
@@ -20,32 +19,35 @@ public class LightwareDataValidator implements AltimeterValidator {
 		// beginning of the buffer. Later, we should probably extract a range of values, 
 		// and average them.
 		
-		if (data.length > 100)
+		int end = 100 < dataSampleBufferSize ? 100 : data.length;
+		byte[] sampleData = Arrays.copyOfRange(data, 0, end);
+		// find the values between the patterns. The value may have up to 2 spaces (0x20) padding
+		int start = KMPMatch.indexOf(sampleData, terminatingPattern);
+		// if we have a match, extract a sample from the next value in the sequence
+		if (start > 0 && (start + dataSampleSize + terminatingPattern.length) < sampleData.length )
 		{
-			byte[] sampleData = Arrays.copyOfRange(data, 0, 100);
-			// find the values between the patterns. The value may have up to 2 spaces (0x20) padding
-			int start = KMPMatch.indexOf(sampleData, terminatingPattern);
-			// if we have a match, extract a sample from the next value in the sequence
-			if (start > 0 && (start + dataSampleSize + terminatingPattern.length) < sampleData.length )
-			{
-				try {
-					byte[] sampleSlice = Arrays.copyOfRange(sampleData, (start + terminatingPattern.length), dataSampleSize + terminatingPattern.length);
-					int matchEnd = KMPMatch.indexOf(sampleSlice, terminatingPattern);
-					if (matchEnd > 0)
-					{
-						byte[] sample = Arrays.copyOfRange(sampleSlice, 0, matchEnd);
-						Log.d(this.getClass().getName(), "Meters: " + new String(sample));
-						meters = Float.parseFloat(new String(sample));
-						if (meters < 0)
-							meters = AltimeterService.ALTIMETER_OUT_OF_RANGE_THRESHOLD;
-					}	
-				} 
-				// TODO - throw an IllegalValidationError, instead of an int
-				catch (IllegalArgumentException iae)
+			int fromIndex = start + terminatingPattern.length;
+			int toIndex = fromIndex + dataSampleSize + terminatingPattern.length;
+			try {
+				byte[] sampleSlice = Arrays.copyOfRange(sampleData, fromIndex, toIndex);
+				int matchEnd = KMPMatch.indexOf(sampleSlice, terminatingPattern);
+				if (matchEnd > 0)
 				{
-					Log.d("LightwareDataValidator.parseDataPayload", "start: " + start);
-					return -1;
-				}
+					byte[] sample = Arrays.copyOfRange(sampleSlice, 0, matchEnd);
+					Log.d(this.getClass().getName(), "Meters: " + new String(sample));
+					meters = Float.parseFloat(new String(sample));
+					if (meters < 0)
+						meters = AltimeterService.ALTIMETER_OUT_OF_RANGE_THRESHOLD;
+				}	
+			} 
+			catch (ArrayIndexOutOfBoundsException aioobe)
+			{
+				Log.d("LightwareDataValidator.parseDataPayload", "start: " + start);
+			}
+			// TODO - throw an IllegalValidationError, instead of an int
+			catch (IllegalArgumentException iae)
+			{
+				Log.d("LightwareDataValidator.parseDataPayload", "illegal argument fromIndex: " + fromIndex + " toIndex: " + toIndex);
 			}
 		}
 		else {
